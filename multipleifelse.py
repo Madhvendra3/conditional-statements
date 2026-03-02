fce33ed3cb6b33338d6946a7f98c909bd3b250a3
Madhvendra3 <rajawatbunty@gmail.com>
2026-03-02 20:46:35 +0000

    Add files via upload

---
+++ multipleifelse.py
@@ -0,0 +1,21 @@
+print("choose ride:")
+print("1 = bike")
+print("2 = car")  
+
+choice = int(input("select your choice"))
+if choice == "1":
+    print("what type of bike are you choosing?")
+    print("choice1 = scooter")
+    print("choice2 = scooty")
+if choice == "scooty":
+    print("you have selected scooty")
+else:
+    print("you have selected scooter")
+if choice == "2":
+    print("what type of car are you choosing?")          
+    print("choice3 = suv")
+    print("choice4 = sports")
+if choice == "suv":
+    print("you have selected suv")
+else: 
+    print("you have selected sports") 
